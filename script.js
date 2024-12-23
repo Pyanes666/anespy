e0565e8d99e84637343de0224b410dbc538c0f9a
Pyanes666 <anes.hammouche.qc@gmail.com>
2024-12-23 18:13:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 function downloadGame() {
     var platform = document.querySelector('input[name="platform"]:checked').value;
     var downloadLink;
+    var fileName;
 
     if (platform === "android") {
         downloadLink = "jeu.apk";
+        fileName = "my-game.apk";
     } else if (platform === "windows") {
         downloadLink = "anes.exe";
+        fileName = "my-game.exe";
     }
 
     var a = document.createElement("a");
     a.href = downloadLink;
-    a.download = true;
+    a.download = fileName;
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
